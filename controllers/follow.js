c2ab3f84a7b33812e832037737e8a662e68f07a7
ChristianAlbaladejo <christian.albaladejo06@gmail.com>
2020-05-31 10:57:56 +0200

    Following

--- controllers/follow.js
+++ controllers/follow.js
@@ -27,7 +27,7 @@ function deleteFollow(req, res) {
     var userId = req.user.sub;
     var followId = req.params.id;
 
-    Follow.find({ 'user': userId, 'followed': followId }).remove( err => {
+    Follow.find({ 'user': userId, 'followed': followId }).remove(err => {
         if (err) return res.status(500).send({ message: 'Error in unfollow' });
         return res.status(200).send({ message: 'Follow is deleted' })
     });
@@ -82,13 +82,48 @@ function getFollowedUsers(req, res) {
         if (err) return res.status(500).send({ message: 'Error in unfollow' });
 
         if (!follows) return res.status(404).send({ message: 'You are not followed any user' })
-
-        return res.status(200).send({
-            total: total,
-            page: Math.ceil(total / itemsPerPage),
-            follows
-        })
+        followUserIds(req.user.sub).then((value) => {
+            return res.status(200).send({
+                total: total,
+                page: Math.ceil(total / itemsPerPage),
+                follows,
+                users_following: value.following,
+                users_followmy: value.followed,
+            })
+        });
     });
+}
+
+async function followUserIds(user_id) {
+    var following = await Follow.find({ "user": user_id }).select({ '_id': 0, '_v': 0, 'user': 0 }).exec().then((follow) => {
+        return follow;
+    }).catch((err) => {
+        return handleError(err);
+    });
+
+    var followed = await Follow.find({ "followed": user_id }).select({ '_id': 0, '_v': 0, 'follower': 0 }).exec().then((follow) => {
+        return follow;
+    }).catch((err) => {
+        return handleError(err);
+    });
+
+    //following ids
+    var following_clean = [];
+
+    following.forEach((follow) => {
+        following_clean.push(follow.followed);
+    });
+
+    //followed ids
+    var followed_clean = [];
+
+    followed.forEach((follow) => {
+        followed_clean.push(follow.followed);
+    });
+    return {
+        following: following_clean,
+        followed: followed_clean
+    }
 }
 
 //Return users list
