ee7368b6e6afa4ebbbc9fdf61f1eadb6b9cb945a
ChristianAlbaladejo <christian.albaladejo06@gmail.com>
2020-06-04 17:53:22 +0200

    Fixing messages

--- routes/message.js
+++ routes/message.js
@@ -5,8 +5,8 @@ var MessageController = require('../controllers/message');
 var api = express.Router();
 var md_auth = require('../middlewares/authentificated')
 
-api.get('/message', md_auth.ensureAuth, MessageController.saveMessage);
-api.post('/my-messages/:page?', md_auth.ensureAuth, MessageController.getRecivedMessages);
+api.post('/message', md_auth.ensureAuth, MessageController.saveMessage);
+api.get('/my-messages/:page?', md_auth.ensureAuth, MessageController.getRecivedMessages);
 api.get('/messages/:page?', md_auth.ensureAuth, MessageController.getEmmitMessages);
 api.get('/unviewed-messages', md_auth.ensureAuth, MessageController.getUnviewedMessages);
 api.get('/set-viewed-messages', md_auth.ensureAuth, MessageController.setViewedMessages);
